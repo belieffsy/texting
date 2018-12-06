4db7cf8c618c01d86a147aeb15fd690412cd73ae
belieffsy <38274401+belieffsy@users.noreply.github.com>
2018-12-06 17:35:49 +0800

    Update wzq.py

--- wzq.py
+++ wzq.py
@@ -8,6 +8,7 @@ from chequer import Chequer
 import functions as f
 from button import Button
 from msg_box import Msg_box
+import time
 
 def run_game():
     pygame.init()
@@ -17,7 +18,7 @@ def run_game():
     pieces = Group()
 
     screen = pygame.display.set_mode((settings.screen_width,settings.screen_height),4,0)
-    chequer = Chequer(screen,pieces)
+    chequer = Chequer(settings,screen,pieces)
     pygame.display.set_caption("五子棋")
     background = pygame.image.load("background3.jpg").convert()
     button_d = Button(screen,"悔棋",32,(65,49),(20,20,90,55))
@@ -26,9 +27,9 @@ def run_game():
     button_r2 = Button(screen, "重新開始", 33, (200, 49), (128, 18, 145, 62))
 
     msg_box_reset = Msg_box(screen,"確定重新開始？","確定","取消")
-    box_black_win = Msg_box(screen, "黑棋獲勝!", "再来", "退出")
-    box_white_win = Msg_box(screen, "白棋獲勝!", "再来", "退出")
-    box_get_draw = Msg_box(screen, "平局!", "再来", "退出")
+    box_black_win = Msg_box(screen, "黑棋獲勝!", "重置", "退出")
+    box_white_win = Msg_box(screen, "白棋獲勝!", "重置", "退出")
+    box_get_draw = Msg_box(screen, "平局!", "重置", "退出")
     while True:
         f.check_events(settings,chequer,pieces,button_d2,button_r2,msg_box_reset)
         #screen.fill((settings.bg_color))
@@ -44,7 +45,7 @@ def run_game():
         f.draw_button_d(button_d,button_d2)
         f.draw_button_r(button_r, button_r2)
 
-        f.draw(settings,msg_box_reset,box_black_win,box_white_win,box_get_draw)
+        f.draw(settings,msg_box_reset,box_black_win,box_white_win,box_get_draw,chequer)
 
 
 
